2fd37921a75f8f02b80ecaf05831c21bc2900f25
dawidmrozinski <mrozinski-dawid.1993@o2.pl>
2022-11-20 13:33:08 +0100

    Initial commit

---
+++ src/main/java/pl/aktywnieisportowo/AktywnieisportowoApplication.java
@@ -0,0 +1,13 @@
+package pl.aktywnieisportowo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AktywnieisportowoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AktywnieisportowoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/aktywnieisportowo/AktywnieisportowoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aktywnieisportowo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AktywnieisportowoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
